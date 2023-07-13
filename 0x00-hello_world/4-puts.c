d0e86660c6cacec451addf3043269f9b7b5c66c8
root <espoir.rose22@gmail.com>
2023-07-13 06:57:43 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the line wih puts function
+ * Return : 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
